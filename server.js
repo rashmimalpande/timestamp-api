c154a6dc755f7106f5b4dbe7861f276d2331f961
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-05 13:17:33 +0000

    show date given in the url

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
-var app = express()
+var app = express();
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/:date', function (req, res) {
+  res.send(req.params);
+});
 
 app.listen(8080);
